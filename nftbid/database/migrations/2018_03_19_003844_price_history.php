fcdfa7a161fffb877b1de65376554578942e2ca0
Omar-Amaro <omar9939@hotmail.com>
2022-02-21 19:35:14 -0700

    21/02/22

---
+++ nftbid/database/migrations/2018_03_19_003844_price_history.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price_history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_nft')->unsigned();
+            $table->foreign('id_nft')->references('id')->on('nfts');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExist('price_history');
+
+    }
+};
